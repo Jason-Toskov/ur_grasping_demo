79178277261d730477bb1f665084b916f8ffe391
jtos0003 <jtos0003@student.monash.edu>
2023-07-21 17:37:09 +1000

    wip: plan scan path pre execution

--- src/box_grasping/demo.py
+++ src/box_grasping/demo.py
@@ -3,10 +3,12 @@
 from pathlib import Path
 
 import rospy
+from moveit_msgs.msg import RobotTrajectory
+from trajectory_msgs.msg import JointTrajectory
 
 from box_grasping.config import Config
 from box_grasping.motion.ur3e_motion import MotionType, Ur3eMover
-from box_grasping.utils.moveit_helper import load_joint_trajectory
+from box_grasping.utils.moveit_helper import joint_dict_to_robot_state, load_joint_trajectory
 from box_grasping.utils.tf_helper import load_yaml
 from box_grasping.visual.realsense import RealSenseProcessor
 
@@ -26,6 +28,36 @@ class GraspDemoMaster:
         self.scan_joints = load_joint_trajectory(
             Path(self.params.paths.root) / self.params.paths.trajectories.scene_scan
         )
+
+    def construct_scan_plan(self, initial_state):
+        scan_joints = load_joint_trajectory(
+            Path(self.params.paths.root) / self.params.paths.trajectories.scene_scan
+        )
+
+        start_joints = [initial_state] + scan_joints
+        target_joints = scan_joints + [initial_state]
+
+        joint_traj_points = []
+        for start, finish in zip(start_joints, target_joints):
+            start_robot_state = joint_dict_to_robot_state(start)
+            self.mover.move_group.set_start_state(start_robot_state)
+            self.mover.move_group.set_joint_value_target(finish)
+            plan = self.mover.unpack_plan(self.mover.move_group.plan())
+            if plan is None:
+                raise ValueError("Scan plan failed!")
+            joint_traj_points += plan.joint_trajectory.points
+            self.mover.move_group.clear_pose_targets()
+
+        plan_overall = RobotTrajectory(
+            joint_trajectory=JointTrajectory(
+                joint_names=plan.joint_trajectory.joint_names,
+                points=joint_traj_points,
+            )
+        )
+
+        self.mover.move_group.set_start_state_to_current_state()
+
+        return plan_overall
 
     def main(self):
         while not rospy.is_shutdown():
